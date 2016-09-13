2ada28d392380172b412ab91f92f35442c5b739b
root <root@jaime.puppetlabs.vm>
2016-09-13 13:32:47 +0000

    push

---
+++ manifest/init.pp
@@ -0,0 +1,19 @@
+class ntp_wrapper {
+  package { 'ntp':
+    ensure => present,
+  }
+
+  file { '/etc/ntp.conf':
+    ensure  => file.
+    group   => 'root',
+    mode    => '0644',
+    owner   => 'root',
+    source  => 'puppet:///modules/ntp_wrapper/ntp.conf',
+    require => Package['ntp'],
+    notify  => Service['ntpd'],
+
+    service { 'ntpd':
+      ensure    => running,
+      enable    => true,
+    }
+  }
